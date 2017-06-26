259c4d92f54d8ea1ffe5ceb23b011f6a297bd362
William Rodrigues de Sousa <rm78369@fiap.com.br>
2017-06-25 23:00:08 -0300

    <testando>

---
+++ aula2/Produto.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+
+namespace curso {
+    class Produto {
+
+        public string nome;
+        public double preco;
+        public int quantidadeEmEstoque;
+
+        public double valorTotalEmEstoque() {
+            return preco * quantidadeEmEstoque;
+        }
+
+        public void realizarEntrada(int quantidade) {
+            quantidadeEmEstoque = quantidadeEmEstoque + quantidade;
+        }
+
+        public void realizarSaida(int quantidade) {
+            quantidadeEmEstoque = quantidadeEmEstoque - quantidade;
+        }
+
+        public override string ToString() {
+            return nome
+            + ", R$"
+            + preco.ToString("F2", CultureInfo.InvariantCulture)
+            + ", "
+            + quantidadeEmEstoque
+            + " unidades, Total: R$"
+            + valorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
+
+        }
+    }
+}
---
+++ aula2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Globalization;
+namespace curso {
+    class Program {
+        static void Main(string[] args) {
+
+            Produto p;
+            int qte;
+
+            p = new Produto();
+
+            Console.WriteLine("Digite os dados do produto: ");
+            Console.Write("Nome: ");
+            p.nome = Console.ReadLine();
+
+            Console.Write("Preço: ");
+            p.preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("Quantidade em Estoque: ");
+            p.quantidadeEmEstoque = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+            Console.WriteLine("Dados do produto: " + p);
+
+            Console.WriteLine();
+            Console.Write("Digite a quantidade de produtos que entraram no estoque: ");
+            qte = int.Parse(Console.ReadLine());
+            p.realizarEntrada(qte);
+
+            Console.WriteLine();
+            Console.WriteLine("Dados atualizados: " + p);
+
+            Console.Write("Digite a quantidade de produtos que saíram do estoque: ");
+            qte = int.Parse(Console.ReadLine());
+            p.realizarSaida(qte);
+
+            Console.WriteLine();
+            Console.WriteLine("Dados atualizados: " + p);
+
+            Console.ReadLine();
+        }
+    }
+}
